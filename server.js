14ec5bac2d55fd1fbaa3c4b262542da6cb550589
nikksan <nikksanus@gmail.com>
2018-03-15 09:57:00 +0200

    First commit

---
+++ server.js
@@ -0,0 +1,158 @@
+const MongoClient = require('mongodb').MongoClient;
+// Database Name
+const dbName = 'weatherApp';
+const url = 'mongodb://localhost:27017';
+const port = 3000;
+
+// Connect using MongoClient
+MongoClient.connect(url, function(err, client) {
+	if(err){
+		return console.log(err);
+	}
+
+	var express = require('express');
+	var app = express();
+	const locationsCollection = client.db(dbName).collection('locations');
+
+	//Get all locations
+	app.get('/locations', function(req, res){
+	  	if(isObjectEmpty(req.query)){
+		  	locationsCollection.find({}).toArray(function(err, results){
+		  		if(err){
+		  			return res.json({error: true })
+		  		}else if(results === null){
+		  			return res.json({error: true })
+		  		}
+
+		  		var locations = [];
+		  		for(var key in results){
+		  			locations.push({
+		  				location: results[key].location,
+			  			latitude: results[key].latitude,
+			  			longitude: results[key].longitude
+		  			});
+		  		}
+
+		  		res.json({error: false, locations: locations});
+		  	});	
+	  	}else if(isValidLatitude(req.query.latitude) && isValidLongtitude(req.query.longitude) && isValidRadius(req.query.radius)){
+	  		var latitudeOffset = req.query.radius * 0.000001619593482036009;
+	  		var longitudeOffset = req.query.radius * 0.000003239186964072018;
+
+	  		// Convert them to numbers
+	  		req.query.latitude = +req.query.latitude;
+	  		req.query.longitude = +req.query.longitude;
+
+	  		locationsCollection.find({}).toArray(function(err, results){
+	  			if(err){
+	  				return res.json({error: true})
+		  		}else if(results === null){
+		  			return res.json({error: true})
+		  		}
+
+		  		var locations = [];
+		  		for(var key in results){
+		  			if( 
+		  				results[key].latitude > req.query.latitude - latitudeOffset &&
+		  				results[key].latitude < req.query.latitude + latitudeOffset && 
+		  				results[key].longitude > req.query.longitude - longitudeOffset && 
+		  				results[key].longitude < req.query.longitude + longitudeOffset
+		  			)
+			  			locations.push({
+			  				location: results[key].location,
+				  			latitude: results[key].latitude,
+				  			longitude: results[key].longitude
+			  			});
+		  		}
+
+		  		res.json({error: false, locations: locations});
+	  		});
+	  	}else{
+	  		res.json({error: true });
+	  	}
+
+	});
+
+	//getet info about a single location
+	/*
+	app.get('/location', function(req, res){
+	  if(!req.query.location){
+	  	res.json({error: true, message: 'You need to supply location parameter.'});
+	  }else{
+	  	locationsCollection.findOne({location: req.query.location}, function(err, result){
+	  		if(err){
+	  			return res.json({error: true})
+	  		}else if(result === null){
+	  			return res.json({error: true, message: 'No such location'})
+	  		}
+
+	  		var location = {
+	  			location: result.location,
+	  			latitude: result.latitude,
+	  			longitude: result.longitude	
+	  		}
+	  		res.json({error: false, location: location});
+	  	});
+	  }
+	});
+	*/
+
+	//Get measurements for single location
+	app.get('/location/measurements', function(req, res){
+		if(!req.query.location){
+	  		res.json({error: true, message: 'Invalid request'});
+		  }else{
+		  	locationsCollection.findOne({location: req.query.location}, function(err, result){
+		  		if(err){
+		  			return res.json({error: true})
+		  		}else if(result === null){
+		  			return res.json({error: true, message: 'No such location'})
+		  		}
+
+		  		if(req.query.from && req.query.to){
+		  			//Swap if nessesary
+		  			if(req.query.from > req.query.to){
+		  				var tmp = req.query.from;
+		  				req.query.from = req.query.to;
+		  				req.query.to = tmp;
+		  			}
+		  			
+
+		  			var measurements = [];
+		  			for(var key in result.measurements){
+		  				if(	result.measurements[key].timestamp >= req.query.from &&
+		  					result.measurements[key].timestamp <= req.query.to)
+		  				{
+		  					measurements.push(result.measurements[key]);
+		  				}
+		  			}
+		  			res.json({error: false, measurements: measurements});
+		  		}else{
+		  			res.json({error: false, measurements: result.measurements});
+		  		}
+		  		
+		  	});
+		  }
+	});
+
+	app.listen(port, function(){
+		console.log('Web server listening on port ' + port);
+	});
+});
+
+// Helper functions
+function isObjectEmpty(obj){
+	return Object.keys(obj).length === 0 && obj.constructor === Object
+}
+
+function isValidLatitude(lat){
+	return lat && lat >= -90 && lat <= 90; 
+}
+
+function isValidLongtitude(lng){
+	return lng && lng >= -180 && lng <= 180; 
+}
+
+function isValidRadius(radius){
+	return radius && radius > 0;
+}
