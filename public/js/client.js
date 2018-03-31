8feb2edb532d64d57b60d3799ccb7b0f3d5de779
nikksan <nikksanus@gmail.com>
2018-03-31 19:23:29 +0300

    Constantly updating locatios

--- public/js/client.js
+++ public/js/client.js
@@ -12,39 +12,56 @@ function initMap(){
 		zoom: 8
 	});	
 
-	// Add locations on the map
+	// Loop for the fetching locations
+	(function fetchLocationsLoop(){
+		fetchLocations(setTimeout(function(){
+			fetchLocationsLoop();
+		}, 3000))
+	})();
+}
+
+function fetchLocations( callback ){
 	API.getLocations({}, function(locations){
 		for(let location of locations){
-			pins[location.location] = {};
+			if(pins[location.location]){ // allready added as a marker
+				if(location.last_measurement){
+					pins[location.location].infowindow.setContent(getContentString(location.last_measurement));
+				}
+			}else{ 
+				pins[location.location] = {};
 
-			pins[location.location].marker = new google.maps.Marker({
-				position: {lat: location.latitude, lng: location.longitude},
-				map: map,
-				title: location.location
-			});
+				pins[location.location].marker = new google.maps.Marker({
+					position: {lat: location.latitude, lng: location.longitude},
+					map: map,
+					title: location.location
+				});
 
-			let content;
-			if(location.last_measurement){
-				let date = new Date(location.last_measurement.timestamp)
-				content = date.toString() + ':' + '<b>' + location.last_measurement.temperature + ' C' + '</b>';
-			}else{
-				content = 'No temperature was ever recorded for this location.';
+				let content;
+				if(location.last_measurement){
+					content  = getContentString(location.last_measurement);
+				}else{
+					content = 'No temperature was ever recorded for this location.';
+				}
+
+				pins[location.location].infowindow = new google.maps.InfoWindow({
+					content: content
+				});
+
+	        	// Add event listener to the marker
+	        	pins[location.location].marker.addListener('click', function() {
+	        		pins[location.location].infowindow.open(map, this);
+	        	});
 			}
-
-			pins[location.location].infowindow = new google.maps.InfoWindow({
-				content: content
-			});
-
-        	// Add event listener to the marker
-        	pins[location.location].marker.addListener('click', function() {
-        		pins[location.location].infowindow.open(map, this);
-        	});
 
         	typeof callback == 'function' ? callback() : void(0);
         }
     });
 }
 
-
+function getContentString(measurement){
+	let date = new Date(measurement.timestamp)
+	let content = date.toString() + ':' + '<b>' + measurement.temperature + ' C' + '</b>';
+	return content;
+}
 
 
